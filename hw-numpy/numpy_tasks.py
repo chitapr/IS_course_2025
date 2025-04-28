bbffe65e9031ce64c0e66ce44fb2b1f616b43cde
chitapr <marommv9@gmail.com>
2025-04-28 21:57:10 +0300

    new files

--- hw-numpy/numpy_tasks.py
+++ hw-numpy/numpy_tasks.py
@@ -1,46 +1,56 @@
 import numpy as np
 
 def uniform_intervals(a, b, n):
-    """1. создает numpy массив - равномерное разбиение интервала от a до b на n отрезков."""
-    pass
+    return np.linspace(a, b, n)
 
-def cyclic123_array(n): 
-    """2. Генерирует numpy массив длины  3𝑛 , заполненный циклически числами 1, 2, 3, 1, 2, 3, 1...."""
-    pass
+def cyclic123_array(n):
+    return np.tile([1, 2, 3], n)
 
 def first_n_odd_number(n):
-    """3. Создает массив первых n нечетных целых чисел"""
-    pass
+    return 2 * np.arange(n) + 1
 
 def zeros_array_with_border(n):
-    """4. Создает массив нулей размера n x n с "рамкой" из единиц по краям."""
-    pass
+    arr = np.zeros((n, n), dtype=float)
+    arr[0, :] = arr[-1, :] = arr[:, 0] = arr[:, -1] = 1.
+    return arr
 
 def chess_board(n):
-    """5. Создаёт массив n x n с шахматной доской из нулей и единиц"""
-    pass
+    idx = np.indices((n, n)).sum(axis=0)
+    return (idx + 1) % 2
 
 def matrix_with_sum_index(n):
-    """6. Создаёт 𝑛 × 𝑛  матрицу с (𝑖,𝑗)-элементами равным 𝑖+𝑗."""
-    pass
+    i, j = np.indices((n, n))
+    return i + j
 
 def cos_sin_as_two_rows(a, b, dx):
-    """7. Вычислите $cos(x)$ и $sin(x)$ на интервале [a, b) с шагом dx, 
-    а затем объедините оба массива чисел как строки в один массив. """
-    pass
+    x = np.arange(a, b, dx)
+    return np.vstack((np.cos(x), np.sin(x)))
 
 def compute_mean_rowssum_columnssum(A):
-    """8. Для numpy массива A вычисляет среднее всех элементов, сумму строк и сумму столбцов."""
-    pass
+    mean = np.mean(A)
+    rows_sum = np.sum(A, axis=0)
+    columns_sum = np.sum(A, axis=1)
+    return mean, rows_sum, columns_sum
 
 def sort_array_by_column(A, j):
-    """ 9. Сортирует строки numpy массива A по j-му столбцу в порядке возрастания."""
-    pass
+    return A[np.argsort(A[:, j])]
 
 def compute_integral(a, b, f, dx, method):
-    """10. Считает определённый интеграл функции f на отрезке [a, b] с шагом dx 3-мя методами:  
-    method == 'rectangular' - методом прямоугольника   
-    method == 'trapezoidal' - методом трапеций   
-    method == 'simpson' - методом Симпсона  
-    """
-    pass
+    x = np.arange(a, b, dx)
+    y = f(x)
+
+    if method == 'rectangular':
+        return np.sum(y) * dx
+
+    elif method == 'trapezoidal':
+        return (y[0] + y[-1] + 2 * np.sum(y[1:-1])) * dx / 2
+
+    elif method == 'simpson':
+        if len(x) % 2 == 0:  # если нечётное число отрезков
+            x = np.append(x, x[-1] + dx)
+            y = f(x)
+        return (dx / 3) * (y[0] + y[-1]
+                           + 4 * np.sum(y[1:-1:2])
+                           + 2 * np.sum(y[2:-2:2]))
+    else:
+        raise ValueError(f"Unknown method: {method}")
